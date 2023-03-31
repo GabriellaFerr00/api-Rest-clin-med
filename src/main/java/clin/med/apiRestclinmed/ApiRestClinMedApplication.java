cadf52d88c88729002b2d6a88445981309be2edd
Gabriella <gabriellalok3@gmail.com>
2023-03-31 10:56:47 -0300

    iniciando o projeto

---
+++ src/main/java/clin/med/apiRestclinmed/ApiRestClinMedApplication.java
@@ -0,0 +1,13 @@
+package clin.med.apiRestclinmed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestClinMedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestClinMedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/clin/med/apiRestclinmed/ApiRestClinMedApplicationTests.java
@@ -0,0 +1,13 @@
+package clin.med.apiRestclinmed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestClinMedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
