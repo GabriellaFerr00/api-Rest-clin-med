45cf4fe8ceee07cb722c2748fa49920ae4391d03
Gabriella <gabriellalok3@gmail.com>
2023-04-04 03:00:43 -0300

    implementando metodo put e delete da entidade medico

--- src/main/java/clin/med/apiRestclinmed/records/DadosListagemMedicos.java
+++ src/main/java/clin/med/apiRestclinmed/records/DadosListagemMedicos.java
@@ -3,13 +3,15 @@ package clin.med.apiRestclinmed.records;
 import clin.med.apiRestclinmed.entities.MedicoEntity;
 import clin.med.apiRestclinmed.enums.EspecialidadesType;
 
-public record DadosListagemMedicos(String nome,
+public record DadosListagemMedicos(Long id,
+                                   String nome,
                                    String email,
                                    String crm,
                                    EspecialidadesType especialidade) {
 
     public DadosListagemMedicos(MedicoEntity medico) {
         this(
+                medico.getId(),
                 medico.getNome(),
                 medico.getEmail(),
                 medico.getCrm(),
---
+++ src/main/resources/db/migration/V4__alter-table-medicos-add-column-ativo.sql
@@ -0,0 +1,3 @@
+alter table medicos_tb add ativo tinyint;
+
+update medicos_tb set ativo = 1;
